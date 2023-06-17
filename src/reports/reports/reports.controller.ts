1100ec7dd6f7fe99034755aa64ad821bdb154251
Geovane Silva <geoscosta@gmail.com>
2023-06-17 01:13:58 -0300

    Finalizando Relatório

--- src/reports/reports/reports.controller.ts
+++ src/reports/reports/reports.controller.ts
@@ -1,17 +1,55 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, MessageEvent, Param, ParseIntPipe, Post, Render, Res, Sse } from '@nestjs/common';
 import { ReportsService } from './reports.service';
+import { Observable, defer, map, repeat, tap } from 'rxjs';
+import { Status } from '@prisma/client';
+import { Response } from 'express';
 
 @Controller('reports')
 export class ReportsController {
     constructor(private reportsService: ReportsService) {}
+
+    @Get('view')
+    @Render('reports')
+    async view() {
+        const report = await this.reportsService.all();
+        return { report };
+    }
 
     @Get()
     all() {
         return this.reportsService.all();
     }
 
+    @Get(':id')
+    findOne(@Param('id', new ParseIntPipe()) id: number) {
+        return this.reportsService.findOne(id);
+    }
+
     @Post()
     request() {
         return this.reportsService.request();
+    }
+
+    @Sse(':id/events')
+    events(
+        @Param('id', new ParseIntPipe()) id: number,
+        @Res() response: Response,
+    ): Observable<MessageEvent> {
+        return defer(() => this.reportsService.findOne(id)).pipe(
+            repeat({
+                delay: 1000,
+            }),
+            tap((report) => {
+                if(report.status === Status.CONCLUIDO || report.status === Status.ERROR) {
+                    setTimeout(() => {
+                        response.end();
+                    }, 1000);
+                }
+            }),
+            map((report) => ({
+                type: 'message',
+                data: report,
+            })),
+        );
     }
 }
--- src/reports/reports/reports.service.ts
+++ src/reports/reports/reports.service.ts
@@ -1,13 +1,17 @@
+import { InjectQueue } from '@nestjs/bull';
 import { Injectable } from '@nestjs/common';
 import { Status } from '@prisma/client';
+import { Queue } from 'bull';
 import { PrismaService } from 'src/prisma/prisma/prisma.service';
 
 @Injectable()
 export class ReportsService {
 
-    constructor(private prismaService: PrismaService) {
-
-    }
+    constructor(
+        private prismaService: PrismaService,
+        @InjectQueue('reports')
+        private reportsQueue: Queue,
+    ) {}
 
     all() {
         return this.prismaService.report.findMany({
@@ -17,19 +21,49 @@ export class ReportsService {
         });
     }
 
-    findOne(id: string) {
-
-    }
-
-    request() {
-        return this.prismaService.report.create({
-            data: {
-                status: Status.PENDENTE,
+    findOne(id: number) {
+        return this.prismaService.report.findUnique({
+            where: {
+                id,
             },
         });
     }
 
-    produce() {
+    async request() {
+        const report = await this.prismaService.report.create({
+            data: {
+                status: Status.PENDENTE,
+            },
+        });
+        await this.reportsQueue.add({ reportId: report.id });
+        return report;
+    }
 
+    async produce(reportId: number) {
+        console.log('produce method');
+        await sleep(Math.random() * 10000);
+
+        await this.prismaService.report.update({
+            where: {
+                id: reportId,
+            },
+            data: {
+                status: Status.PROCESSANDO,
+            },
+        });
+
+        await sleep(Math.random() * 10000);
+        const randomStatus = Math.random() > 0.5 ? Status.CONCLUIDO : Status.ERROR;
+        await this.prismaService.report.update({
+            where: {
+                id: reportId,
+            },
+            data: {
+                filename: randomStatus === Status.CONCLUIDO ? `report-${reportId}.pdf` : null,
+                status: randomStatus,
+            },
+        });
     }
 }
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
